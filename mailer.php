a1a6b52434af3a6e11119c631a55ded6e5208583
rakesh <rakeshcj333@gmail.com>
2020-08-07 09:55:51 +0530

    first update

--- mailer.php
+++ mailer.php
@@ -9,15 +9,15 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
     <body>
-        <?php              
+        <?php
           require 'PHPMailer/PHPMailerAutoload.php';
           $mail = new PHPMailer;
 
             $mail->isSMTP();                            // Set mailer to use SMTP
             $mail->Host = 'smtp.gmail.com';             // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                     // Enable SMTP authentication
-            $mail->Username = 'rakcraze@gmail.com';          // SMTP username
-            $mail->Password = 'rak333rak333'; // SMTP password
+            $mail->Username = '';          // SMTP username
+            $mail->Password = ''; // SMTP password
             $mail->SMTPSecure = 'tls';                  // Enable TLS encryption, `ssl` also accepted
             $mail->Port = 587;                          // TCP port to connect to
 
